1ab467c7e22aeb6e2cbaf07537834836dcf5df86
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2023-10-13 16:04:50 -0300

    Views

--- criando-aplicacao-mvc/app/Http/Controllers/SeriesController.php
+++ criando-aplicacao-mvc/app/Http/Controllers/SeriesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request) : string
+    public function index(Request $request)
     {
         // return $request->get('id'); buscar o dado em qualquer lugar do request
         // return redirect('https://www.google.com'); redirecionar
@@ -19,12 +19,6 @@ class SeriesController extends Controller
 
         $html = '<ul>';
 
-        foreach ($series as $series) {
-            $html .= "<li>$series</li>";
-        }
-
-        $html .= '</ul>';
-
-        return $series;
+        return view('series.index')->with('series', $series);
     }
 }
---
+++ criando-aplicacao-mvc/resources/views/series/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <form action="">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome">
+    </form>
+</x-layout>
