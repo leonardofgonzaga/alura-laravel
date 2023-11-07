a1f4d8f923e683394e0849f10d3048b33c04db34
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2023-11-07 20:13:42 -0300

    Listando eiposódios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +26,12 @@ Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+
+Route::post('/seasons/{season}/episodes', function(Request $request) {
+    dd($request->all());
+});
 
 // Route::post('/series/destroy/{id}', [SeriesController::class, 'destroy'])
 //     ->name('series.destroy');
