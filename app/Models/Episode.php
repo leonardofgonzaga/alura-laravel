bf555925ee4309166094ce9f8fe35c447cd6b503
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2023-11-08 20:16:21 -0300

    Criando a migration para criar o campo watched

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -8,8 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Episode extends Model
 {
     use HasFactory;
+    
     public $timestamps = false;
     protected $fillable = ['number'];
+    protected $casts = ['watched' => 'boolean'];
 
     public function season()
     {
