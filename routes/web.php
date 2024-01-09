a0928047600bd082843870903df9b139468bd183
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-01-09 20:07:41 -0300

    E-mails, eventos assíncronos, uploads e testes: criando e-mail

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Mail\SeriesCreated;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,6 +38,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/email', function() {
+    return new \App\Mail\SeriesCreated(
+        'Série de teste',
+        1,
+        5,
+        10
+    );
 });
 
 require __DIR__.'/auth.php';
