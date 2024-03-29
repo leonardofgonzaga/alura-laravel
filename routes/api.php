3dd9eee0e375feba1fc4108b3076de2b3ef515ac
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-03-28 21:14:29 -0300

    Laravel: construindo APIs: assistindo um episodio

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\SeriesController;
+use App\Models\Episode;
 use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,13 @@ Route::get('/series/{series}/seasons', function(Series $series) {
 
 Route::get('/series/{series}/episodes', function(Series $series) {
     return $series->episodes;
+});
+
+Route::patch('/episodes/{episode}', function(Episode $episode, Request $request) {
+    $episode->watched = $request->watched;
+    $episode->save();
+
+    return $episode;
 });
 // Route::get('/series', [SeriesController::class, 'index']);
 // Route::post('/series', [SeriesController::class, 'store']);
