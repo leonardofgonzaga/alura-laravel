e474a646a84214b8a211e86f7dc6562414bebd0f
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-03-28 21:28:56 -0300

    Laravel: construindo APIs: acessors e mutators

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Season;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Episode extends Model
@@ -24,5 +25,13 @@ class Episode extends Model
     // public function scopeWatched(Builder $query)
     // {
     //     $query->where('watched', true);
+    // }
+
+    // protected function watched(): Attribute 
+    // {
+    //     return new Attribute(
+    //         get: fn ($watched) => (bool) $watched,
+    //         set: fn ($watched) => (bool) $watched
+    //     );
     // }
 }
