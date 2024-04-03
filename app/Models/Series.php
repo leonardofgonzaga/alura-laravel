26b94db2d598571be32e5d635124eb58b3b8d131
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-04-02 21:04:34 -0300

    Laravel: construindo APIs: navegação e HateOAS

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Season;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Series extends Model
@@ -16,6 +17,7 @@ class Series extends Model
     protected $fillable = ['name', 'cover'];
     // protected $primaryKey = 'id';
     // protected $with = ['seasons']; Buscar séries já com as temporadas passando o relacionamento
+    protected $appends = ['links'];
 
     public function seasons()
     {
@@ -33,5 +35,25 @@ class Series extends Model
         self::addGlobalScope('ordered', function (Builder $queryBuilder) {
             $queryBuilder->orderBy('name');
         });
+    }
+
+    public function links(): Attribute 
+    {
+        return new Attribute(
+            get: fn() => [
+                [
+                    'rel' => 'self',
+                    'url' => "/api/series/{$this->id}"
+                ],
+                [
+                    'rel' => 'seasons',
+                    'url' => "/api/series/{$this->id}/seasons"
+                ],
+                [
+                    'rel' => 'self',
+                    'url' => "/api/series/{$this->id}/episodes"
+                ],
+            ]
+        );
     }
 }
